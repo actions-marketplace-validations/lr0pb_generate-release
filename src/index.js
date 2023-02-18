8f75a32a37cf1504a51628c5b2305ce7ff5f8508
Igor <hello.lr0pb@gmail.com>
2023-02-18 19:12:15 +0200

    Fix check what files changed during push

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-import { setOutput, setFailed } from '@actions/core';
+import { getInput, setOutput, setFailed } from '@actions/core';
 import { context, rest } from '@actions/github';
 import { readFileSync } from 'fs';
-import { join } from 'path';
+
+function isPackageChanged(packagePath) {
+  const changedFiles = context.payload.commits
+    .reduce((files, commit) => {
+      return files.push(...commit.modified);
+    }, []);
+  console.log(changedFiles);
+
+  return changedFiles.includes(packagePath);
+}
 
 async function run() {
   try {
-    const packageJsonPath = join(process.cwd(), 'package.json');
-    const packageJson = JSON.parse(readFileSync(packageJsonPath));
+    const basePath = process.env.GITHUB_WORKSPACE;
+    const packagePath = getInput('package-path').replace(/^\//, '');
+    const isChanged = isPackageChanged(packagePath);
 
-    const modified = context.payload.head_commit.modified || [];
-    console.log(context.payload.head_commit);
-    if (!modified) {
-      console.log('No files was modified during commit (what?)');
+    if (!isChanged) {
+      console.log('No changes in package.json');
       return;
     }
-    const changedFiles = modified.filter(file => {
-      return file === 'package.json';
-    });
-    console.log(changedFiles);
 
-    if (changedFiles.length === 0) {
-      console.log('No changes to package.json. Skipping release.');
-      return;
-    }
+    const packageJson = JSON.parse(
+      readFileSync(`${basePath}${packagePath}`, 'utf8')
+    );
 
     const newVersion = packageJson.version;
     console.log(`New version: ${newVersion}`);
@@ -33,6 +36,7 @@ async function run() {
       method: 'GET',
       url: diffUrl
     });
+    console.log(diffContent);
 
     if (isVersionChanged(diffContent.data, newVersion)) {
       console.log('Release happened!');
